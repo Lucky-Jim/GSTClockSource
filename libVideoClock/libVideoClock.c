1b6509bbcd00c1c4571c39e7e1c78c3a176b43dd
Tom Burnley <tburnley@cisco.com>
2016-10-25 13:38:50 +0100

    Attempt at a plugin

--- libVideoClock/libVideoClock.c
+++ libVideoClock/libVideoClock.c
@@ -1,6 +1,88 @@
 #include "libVideoClock.h"
 
-void
-print_hello(){
-  printf("$(message)\n");
+G_DEFINE_TYPE (GstMyFilter, gst_my_filter, GST_TYPE_ELEMENT);
+
+
+static GstStaticPadTemplate sink_factory =
+GST_STATIC_PAD_TEMPLATE (
+  "sink",
+  GST_PAD_SINK,
+  GST_PAD_ALWAYS,
+  GST_STATIC_CAPS ("ANY")
+);
+
+static GstStaticPadTemplate source_factory =
+GST_STATIC_PAD_TEMPLATE (
+  "source",
+  GST_PAD_SOURCE,
+  GST_PAD_ALWAYS,
+  GST_STATIC_CAPS ("ANY")
+);
+
+static gboolean
+plugin_init (GstPlugin *plugin)
+{
+  return gst_element_register (plugin, "my_filter",
+			       GST_RANK_NONE,
+			       GST_TYPE_MY_FILTER);
+}
+
+GST_PLUGIN_DEFINE (
+  GST_VERSION_MAJOR,
+  GST_VERSION_MINOR,
+  my_filter,
+  "My filter plugin",
+  plugin_init,
+  VERSION,
+  "LGPL",
+  "GStreamer",
+  "http://gstreamer.net/"
+)
+
+
+static void
+gst_my_filter_class_init (GstMyFilterClass * klass)
+{
+  GstElementClass *element_class = GST_ELEMENT_CLASS (klass);
+
+  gst_element_class_set_static_metadata (element_klass,
+    "An example plugin",
+    "Example/FirstExample",
+    "Shows the basic structure of a plugin",
+    "your name <your.name@your.isp>");
+
+
+  gst_element_class_add_pad_template (element_class,
+  	gst_static_pad_template_get (&src_factory));
+
+
+  gst_element_class_add_pad_template (element_class,
+  	gst_static_pad_template_get (&sink_factory));
+
+}
+
+
+static void
+gst_my_filter_init (GstMyFilter *filter)
+{
+  /* pad through which data comes in to the element */
+  filter->sinkpad = gst_pad_new_from_static_template (
+	&sink_template, "sink");
+  /* pads are configured here with gst_pad_set_*_function () */
+
+
+
+  gst_element_add_pad (GST_ELEMENT (filter), filter->sinkpad);
+
+  /* pad through which data goes out of the element */
+  filter->srcpad = gst_pad_new_from_static_template (
+	&src_template, "src");
+  /* pads are configured here with gst_pad_set_*_function () */
+
+
+
+  gst_element_add_pad (GST_ELEMENT (filter), filter->srcpad);
+
+  /* properties initial value */
+  filter->silent = FALSE;
 }
